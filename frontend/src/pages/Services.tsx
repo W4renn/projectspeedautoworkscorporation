7336f07faa6ca7f408201eef4bc60a6bb3617aa7
W4renn <dwiwoiii2003@gmail.com>
2026-05-13 13:23:39 +0800

    Update Services.tsx

--- frontend/src/pages/Services.tsx
+++ frontend/src/pages/Services.tsx
@@ -56,7 +56,7 @@ const Services: React.FC = () => {
             <img 
                           src={`${BASE_IMAGE_URL}/${service.imgURL || ''}`} 
                           alt={service.title} 
-                          className="product-image"
+                          className="service-image"
                           onError={(e) => {
                             e.currentTarget.src = '/vite.svg'; // fallback
                             e.currentTarget.alt = 'Image not available';
